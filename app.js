ec97e4138b7e3c7e36a526c27dbcf4d41dd5e73b
DevOjeda016 <172947644+DevOjeda016@users.noreply.github.com>
2025-08-22 00:44:24 -0600

    fix: handle paths to serve static file

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
 const express = require('express')
+const path = require('path')
 const app = express()
 
 // get the port from env variable
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
